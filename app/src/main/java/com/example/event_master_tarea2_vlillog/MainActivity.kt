67e91b9b4a886ca43a0198c558217805017a1c04
Neovix33 <112022312+Neovix33@users.noreply.github.com>
2026-04-20 12:47:20 -0400

    Cambioo de icono

--- app/src/main/java/com/example/event_master_tarea2_vlillog/MainActivity.kt
+++ app/src/main/java/com/example/event_master_tarea2_vlillog/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.Navigation
 import com.example.event_master_tarea2_vlillog.ui.theme.Event_Master_Tarea2_VLilloGTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,16 +20,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Event_Master_Tarea2_VLilloGTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
